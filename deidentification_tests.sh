b740c9f27e97f2f70a23618684f44d335952b1ca
Paul M. Heider <heiderp@musc.edu>
2021-04-01 09:20:24 -0400

    new vars: ots_root, log, short_corpus, _system; new system: nlm scrubber
    New environment variables for:
    - OTS_DIR the root folder for all scripts
    - LOG_DIR for writing log events
    - SHORT_CORPUS for a corpus moniker handy for writing in log files
    - SHORT_SYSTEM for a system moniker handy for writing in log files
    
    Added NLM Scrubber as a second system in the script after CliniDeID

--- deidentification_tests.sh
+++ deidentification_tests.sh
@@ -1,12 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
 
-if [[ -z $ETUDE_DIR ]]; then
-    echo "The variable \$ETUDE_DIR is not set"
+if [[ -z $OTS_DIR ]]; then
+    echo "The variable \$OTS_DIR is not set. It should be set to the directory containing this script."
     exit 0
 fi
 
-if [[ -z $ETUDE_CONFIGS ]]; then
-    echo "The variable \$ETUDE_CONFIGS is not set"
+if [[ -z $ETUDE_DIR ]]; then
+    echo "The variable \$ETUDE_DIR is not set"
     exit 0
 fi
 
@@ -17,25 +17,55 @@ else
     echo "Using '${OUTPUT_ROOT}' as root directory for output"
 fi
 
-mkdir -p ${OUTPUT_ROOT}/logs
+export LOG_DIR=${OUTPUT_ROOT}/logs
+mkdir -p ${LOG_DIR}
 		      
-if [[ -n $CLINIDEID_ROOT ]]; then
-    echo "CliniDeID"
-    if [[ -n $CORPUS2014 ]]; then
-	echo "2014 i2b2 Corpus"
+if [[ -n $CORPUS2014 ]]; then
+    echo "2014 i2b2 Corpus"
+    export SHORT_CORPUS=2014_train
+    if [[ -n $CLINIDEID_ROOT ]]; then
+	echo "CliniDeID"
+	export SHORT_SYSTEM=clinideid
 	cd ${CLINIDEID_ROOT}
 	export OUTPUT_DIR=${OUTPUT_ROOT}/clinideid_cli
 	mkdir -p ${OUTPUT_DIR}
 	time ./runCliniDeIDcommandLine.sh \
-	     --inputFile --inputDir "$CORPUS2014/2014deidCorrectionsYoung/train/txt" \
+	     --inputFile --inputDir "$CORPUS2014/train/txt" \
 	     --outputFile --outputDir ${OUTPUT_DIR} \
 	     --level beyond \
 	     --outputTypes filtered \
-	     1> ${OUTPUT_ROOT}/logs/2014_train.stdout \
-	     2> ${OUTPUT_ROOT}/logs/2014_train.stderr
+	     1> ${LOG_ROOT}/${SHORT_SYSTEM}_${SHORT_CORPUS}.stdout \
+	     2> ${LOG_ROOT}/${SHORT_SYSTEM}_${SHORT_CORPUS}.stderr
+    else
+	echo "Skipping CliniDeID"
     fi
+    ####
+    ## NLM Scrubber
+    ####
+    if [[ -n $SCRUBBER_ROOT ]]; then
+	echo "NLM Scrubber"
+	export SHORT_SYSTEM=scrubber
+	cd ${SCRUBBER_ROOT}
+	export OUTPUT_DIR=${OUTPUT_ROOT}/${SHORT_SYSTEM}
+	mkdir -p ${OUTPUT_DIR}
+	#time ./scrubber.19.0403.lnx \
+	#    $OTS_DIR/nlm-scrubber/i2b2_2014_train.conf \
+	#    1> $LOG_DIR/scrubber_2014_train.stdout \
+	#    2> $LOG_DIR/scrubber_2014_train.stderr
+	if [[ -z $CORPUS_UTILS ]]; then
+	    echo "Skipping nlm2brat conversion because the variable \$CORPUS_UTILS is not set.  It is available here:  https://github.com/MUSC-TBIC/corpus-utils.git"
+	else
+	    python3 ${CORPUS_UTILS}/nlm-scrubber/nlm2brat.py \
+		--raw-dir ${CORPUS2014}/train/txt \
+		--processed-dir ${OUTPUT_DIR} \
+		--output-dir ${OUTPUT_DIR}_brat
+	fi
+    else
+	echo "Skipping NLM Scrubber"
+    fi
+
 else
-    echo "Skipping CliniDeID"
+    echo "Skipping 2014 i2b2 Corpus"
 fi
 
 if [[ -n $CORPUS2016 ]]; then
